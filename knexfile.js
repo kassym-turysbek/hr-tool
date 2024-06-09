8aa20653001aecdef582272ee7456d84d9033d04
Kassym Turysbek <91659281+kassymturysbek@users.noreply.github.com>
2024-06-10 04:22:31 +0500

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,7 @@
 module.exports = {
     development: {
         client: 'pg',
-       connection: {
-            connectionString: 'postgres://hr_tool_db_user:IekMEYqzqciFT72K2nEyCFiUr5YZKJ7M@dpg-cpis6pq1hbls73bm7dl0-a/hr_tool_db',
-            ssl: { rejectUnauthorized: false } // This line might be needed if your database requires SSL
-           
-        },
+         connection: process.env.DATABASE_URL,
         migrations: {
             directory: __dirname + '/db/migrations',
         },
@@ -19,10 +15,7 @@ module.exports = {
     },
     production: {
         client: 'pg',
-        connection: {
-            connectionString: 'postgres://hr_tool_db_user:IekMEYqzqciFT72K2nEyCFiUr5YZKJ7M@dpg-cpis6pq1hbls73bm7dl0-a/hr_tool_db',
-            ssl: { rejectUnauthorized: false } // This line might be needed if your database requires SSL
-        },
+          connection: process.env.DATABASE_URL,
         migrations: {
             directory: __dirname + '/db/migrations',
         },
