3ef545f35c8997a6352ecff4592036306ecb75ba
Kassym Turysbek <91659281+kassymturysbek@users.noreply.github.com>
2024-06-10 04:18:13 +0500

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -2,8 +2,9 @@ module.exports = {
     development: {
         client: 'pg',
        connection: {
-            connectionString: 'postgres://hr_tool_db_user:IekMEYqzqciFT72K2nEyCFiUr5YZKJ7M@dpg-cpis6pq1hbls73bm7dl0-a.oregon-postgres.render.com/hr_tool_db',
+            connectionString: 'postgres://hr_tool_db_user:IekMEYqzqciFT72K2nEyCFiUr5YZKJ7M@dpg-cpis6pq1hbls73bm7dl0-a/hr_tool_db',
             ssl: { rejectUnauthorized: false } // This line might be needed if your database requires SSL
+           
         },
         migrations: {
             directory: __dirname + '/db/migrations',
@@ -19,7 +20,7 @@ module.exports = {
     production: {
         client: 'pg',
         connection: {
-            connectionString: 'postgres://hr_tool_db_user:IekMEYqzqciFT72K2nEyCFiUr5YZKJ7M@dpg-cpis6pq1hbls73bm7dl0-a.oregon-postgres.render.com/hr_tool_db',
+            connectionString: 'postgres://hr_tool_db_user:IekMEYqzqciFT72K2nEyCFiUr5YZKJ7M@dpg-cpis6pq1hbls73bm7dl0-a/hr_tool_db',
             ssl: { rejectUnauthorized: false } // This line might be needed if your database requires SSL
         },
         migrations: {
