6145002bc89e1e527cb88763fb49e6a7790547d2
Kassym Turysbek <91659281+kassymturysbek@users.noreply.github.com>
2024-06-10 03:10:10 +0500

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,7 @@
 module.exports = {
     development: {
         client: 'pg',
-        connection: {
-            database: "hr_tool_db",
-            host: "dpg-cpis6pq1hbls73bm7dl0-a",
-            user: "hr_tool_db_user",
-            password: "IekMEYqzqciFT72K2nEyCFiUr5YZKJ7M"
-        },
+       connection: process.env.DATABASE_URL,
         migrations: {
             directory: __dirname + '/db/migrations',
         },
